9eb3c52636966730dba1a87847fe0d2b8ccface6
Jace Hlinak <jhlinak@charlotte.edu>
2026-05-04 20:59:02 -0400

    update seed script

--- prisma/seed.js
+++ prisma/seed.js
@@ -4,43 +4,35 @@ import prisma from '../src/config/db.js';
 const isDev = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
 
 try {
-  if(isDev) {
-    await prisma.$queryRaw`TRUNCATE items, users, orders, order_items, reviews RESTART IDENTITY CASCADE;`;
+  await prisma.$queryRaw`TRUNCATE items, users, orders, order_items, reviews RESTART IDENTITY CASCADE;`;
+
+  const usersData = [
+    { email: 'user@test.com', password: 'test1234' },
+    { email: 'admin@test.com', password: 'test1234', role: 'ADMIN' },
+  ];
+
+  const users = [];
+
+  for (const userData of usersData) {
+    const hashedPassword = await bcrypt.hash(userData.password, 10);
+
+    const user = await prisma.user.create({
+      data: {
+        email: userData.email,
+        password: hashedPassword,
+        role: userData.role || 'USER',
+      },
+    });
+    users.push(user);
   }
 
-  const userCount = await prisma.user.count();
-  if(userCount === 0) {
-    const usersData = [
-      { email: 'user@test.com', password: 'test1234' },
-      { email: 'admin@test.com', password: 'test1234', role: 'ADMIN' },
-    ];
-
-    const users = [];
-  
-    for (const userData of usersData) {
-      const hashedPassword = await bcrypt.hash(userData.password, 10);
-
-      const user = await prisma.user.create({
-        data: {
-          email: userData.email,
-          password: hashedPassword,
-          role: userData.role || 'USER',
-        },
-      });
-      users.push(user);
-    }
-  }
-
-  const itemCount = await prisma.item.count();
-  if(itemCount === 0) {
-    const itemsData = [
-      { name: "Television", price: 500 },
-      { name: "Desk", price: 100 },
-      { name: "Chair", price: 75},
-      { name: "Laptop", price: 800}
-    ]
-    await prisma.item.createMany({ data: itemsData });
-  }
+  const itemsData = [
+    { name: "Television", price: 500 },
+    { name: "Desk", price: 100 },
+    { name: "Chair", price: 75},
+    { name: "Laptop", price: 800}
+  ]
+  await prisma.item.createMany({ data: itemsData });
 
   console.log('Seed completed successfully!');
 } catch (error) {
